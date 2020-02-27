0be27c46456bd1d1d5d08e151ec9325f9a585f04
Spuny <spuny4@gmail.com>
2020-02-27 11:18:29 +0100

    Moved

--- tools/karmada.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh k8s-master-1 sudo docker kill armada
--- tools/sarmada.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh k8s-master-1 sudo docker run -d --rm --net host  --name armada -v /etc/:/etc/ -v /home/ubuntu/.kube/:/home/ubuntu/.kube/ -v /home/ubuntu/:/tmp/ quay.io/airshipit/armada:latest
